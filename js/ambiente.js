fe3c4771f1bda377a9c0a2b9d2884c7eceec642d
Henrique <henriqueventurini.xp@gmail.com>
2026-07-23 10:00:32 -0300

    estrutura de repeticao

---
+++ js/ambiente.js
@@ -0,0 +1,19 @@
+let contador = 1;
+
+// function Ola() {
+//     while(contador <= 10){
+//         console.log('Olá mundo!');
+//         contador++;
+//     }
+// }
+
+// Ola();
+
+function tudoBem() {
+    do {
+        console.log('Tudo bem? ' + contador);
+        contador++;
+    } while (contador <= 10);
+}
+
+tudoBem()
