35906188660f24e0a732343b040961bb7128070f
Henrique <henriqueventurini.xp@gmail.com>
2026-07-27 17:37:47 -0300

    entendendo arrays

---
+++ js/ex016.js
@@ -0,0 +1,18 @@
+let barril = [5, 8, 2, 9 , 3];
+
+console.log(barril);
+
+barril.sort();
+
+console.log(barril);
+
+// for (let index = 0; index < barril.length; index++) {
+//     console.log(barril[index]);
+
+
+for(let i in barril){
+    console.log(barril[i]);
+    
+}
+
+console.log(barril.indexOf(9));
